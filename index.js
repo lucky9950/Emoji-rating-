ef90d7a527bfad1782c4852c1f6f02af1a7f5060
Lakhan Bichukale <lakhanb0508@gmail.com>
2023-07-23 10:36:40 +0530

    Emoji-review

---
+++ index.js
@@ -0,0 +1,29 @@
+const starsEl=document.querySelectorAll(".fa-star")
+
+const emojisEl= document.querySelectorAll(".far")
+
+const colorsAr=["red","orange","lightblue","lightgreen","green"]
+
+updateRating(0);
+
+starsEl.forEach((starsEl,index)=>{
+    starsEl.addEventListener("click",()=>{
+       updateRating(index)
+    })
+})
+
+function updateRating(index){
+    starsEl.forEach((starsEl,idx)=>{
+        if(idx<index+1){
+            starsEl.classList.add("active");
+        }else{
+            starsEl.classList.remove("active")
+        }
+
+    });
+
+    emojisEl.forEach((emojiEl)=>{
+        emojiEl.style.transform = `translateX(-${index*50}px)`;
+        emojiEl.style.color = colorsAr[index]
+    });
+}
